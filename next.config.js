b369523b7af1a506006201eac27fe61afebdd3fb
danielfbustos3 <danielfbustos3@gmail.com>
2021-10-12 17:59:40 -0500

    deleted config.js

--- next.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  reactStrictMode: true,
-  env: {
-    MONGO_URI:
-      "mongodb+srv://danielfbustos3:Futbol123@futbolp.1ygb1.mongodb.net/test?retryWrites=true&w=majority",
-  },
-};
