8d237ab246ce0ea9b6ab771d30ee6eddc1f9b669
danielfbustos3 <danielfbustos3@gmail.com>
2021-10-19 18:13:32 -0500

    signin complete

--- components/Signins.js
+++ components/Signins.js
@@ -1,4 +1,7 @@
-import styled, { keyframes } from "styled-components";
+// import { keyframes } from "styled-components";
+import { keyframes } from "@emotion/react";
+import styled from "@emotion/styled";
+import { useState } from "react";
 
 const rotate = keyframes`
   0% {
@@ -6,6 +9,39 @@ const rotate = keyframes`
   }
   100% {
     transform: rotate(360deg);
+  }
+`;
+
+const btnanim1 = keyframes`
+0% {
+  left: -100%;
+}
+  50%,100% {
+    left: 100%;
+  }
+`;
+const btnanim2 = keyframes`
+0% {
+    top: -100%;
+  }
+  50%,100% {
+    top: 100%;
+  }
+`;
+const btnanim3 = keyframes`
+0% {
+    right: -100%;
+  }
+  50%,100% {
+    right: 100%;
+  }
+`;
+const btnanim4 = keyframes`
+0% {
+    bottom: -100%;
+  }
+  50%,100% {
+    bottom: 100%;
   }
 `;
 
@@ -21,7 +57,7 @@ export const Container = styled.div`
     rgba(1, 147, 86, 1) 0%,
     rgba(10, 201, 122, 1) 100%
   ); */
-  background: #000;
+  background: #051f14;
 `;
 
 const CardShadow = styled.div`
@@ -37,7 +73,7 @@ const CardShadow = styled.div`
   background-image: linear-gradient(120deg, #faffe4, #01bf71 43%, #340096);
   opacity: 1;
   transition: opacity 0.5s;
-  animation: ${rotate} 15s linear infinite;
+  animation: ${rotate} 5s linear infinite;
   z-index: 1;
 `;
 
@@ -52,8 +88,11 @@ const FormWrap = styled.div`
   box-sizing: border-box;
 
   .cardBorder {
-    width: 45vh;
-    height: 65vh;
+    width: 80vw;
+    max-width: 15rem;
+    height: 80vh;
+    min-height: 19rem;
+    max-height: 22rem;
     position: relative;
     border-radius: 10px;
     background: black;
@@ -66,12 +105,13 @@ const FormWrap = styled.div`
 
     &:before {
       content: "";
-      width: 260%;
-      height: 220%;
+      width: 210%;
+      height: 140%;
+      min-height: 26rem;
       background-image: linear-gradient(135deg, #faffe4, #01bf71 55%, #340096);
       position: absolute;
-      top: -32.5vh;
-      left: -22.5vh;
+      top: -5rem;
+      left: -8rem;
       animation: ${rotate} 1.5s linear infinite;
     }
   }
@@ -84,40 +124,118 @@ const FormWrap = styled.div`
     height: 100%;
     background: linear-gradient(#0d0d0d 65%, #051f14);
     padding: 40px;
-    h2 {
-      margin: 0 0 30px;
+    .icon {
+      position: absolute;
+      width: 100%;
+      left: 0;
+      top: 0;
+      padding: 2rem;
+      text-align: center;
+      text-decoration: none;
+      color: #fff;
+      font-weight: 700;
+      font-size: 1.7rem;
+      &:hover {
+        transition: all 0.2s ease-in-out;
+        color: #01bf71;
+        transform: scale(1.005);
+      }
+    }
+    p {
+      margin: 4rem 0 30px;
       padding: 0;
       color: #fff;
-      text-align: center;
+      font-size: 1rem;
+      cursor: default;
     }
     .user-box {
       position: relative;
       input {
         width: 100%;
         padding: 10px 0;
-        font-size: 16px;
+        font-size: 0.8rem;
         color: #fff;
-        margin-bottom: 30px;
+        margin-bottom: 50px;
         border: none;
         border-bottom: 1px solid #fff;
         outline: none;
         background: transparent;
-        &:focus label {
-          top: -20px;
-          left: 0;
-          color: #03e9f4;
-          font-size: 12px;
-        }
       }
-      label {
+      .inlabel {
         position: absolute;
-        top: 0;
+        top: -0.5rem;
         left: 0;
+        color: #01bf71;
+        font-size: 0.5rem;
+        transition: 0.2s;
+      }
+      .offlabel {
+        position: absolute;
+        top: 0.1rem;
+        left: 0.2rem;
         padding: 10px 0;
-        font-size: 16px;
+        font-size: 0.7rem;
         color: #fff;
         pointer-events: none;
         transition: 0.5s;
+      }
+    }
+    .submitBtn {
+      position: relative;
+      display: inline-block;
+      padding: 10px 20px;
+      color: #01bf71;
+      font-size: 0.7rem;
+      text-decoration: none;
+      text-transform: uppercase;
+      overflow: hidden;
+      transition: 0.5s;
+      letter-spacing: 4px;
+      &:hover {
+        background: #01bf71;
+        color: #fff;
+        border-radius: 5px;
+        box-shadow: 0 0 0.3rem #01bf71, 0 0 1rem #01bf71, 0 0 2rem #01bf71,
+          0 0 3rem #01bf71;
+      }
+    }
+    span {
+      position: absolute;
+      display: block;
+      &:nth-child(1) {
+        top: 0;
+        left: -100%;
+        width: 100%;
+        height: 2px;
+        background: linear-gradient(90deg, transparent, #01bf71);
+        animation: ${btnanim1} 1s linear infinite;
+      }
+      &:nth-child(2) {
+        top: -100%;
+        right: 0;
+        width: 2px;
+        height: 100%;
+        background: linear-gradient(180deg, transparent, #01bf71);
+        animation: ${btnanim2} 1s linear infinite;
+        animation-delay: 0.25s;
+      }
+      &:nth-child(3) {
+        bottom: 0;
+        right: -100%;
+        width: 100%;
+        height: 2px;
+        background: linear-gradient(270deg, transparent, #01bf71);
+        animation: ${btnanim3} 1s linear infinite;
+        animation-delay: 0.5s;
+      }
+      &:nth-child(4) {
+        bottom: -100%;
+        left: 0;
+        width: 2px;
+        height: 100%;
+        background: linear-gradient(360deg, transparent, #01bf71);
+        animation: ${btnanim4} 1s linear infinite;
+        animation-delay: 0.75s;
       }
     }
   }
@@ -134,7 +252,7 @@ const FormWrap = styled.div`
 //   }
 // `;
 
-export const Icon = styled.a`
+const Icon = styled.a`
   margin-left: 32px;
   margin-top: 32px;
   text-decoration: none;
@@ -153,73 +271,32 @@ export const Icon = styled.a`
   }
 `;
 
-export const FormContent = styled.div`
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-
-  @media screen and (max-width: 480px) {
-    padding: 10px;
-  }
-`;
-
-export const Form = styled.form`
-  background: #010101;
-  max-width: 400px;
-  height: auto;
-  width: 100%;
-  z-index: 1;
-  display: grid;
-  margin: 0 auto;
-  padding: 80px 32px;
-  border-radius: 4px;
-  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.9);
-
-  @media screen and (max-width: 400px) {
-    padding: 32px 32px;
-  }
-`;
-
-export const FormH1 = styled.h1`
-  margin-bottom: 40px;
-  color: #fff;
-  font-size: 20px;
-  font-weight: 400;
-  text-align: center;
-`;
-
-export const FormLabel = styled.label`
-  margin-bottom: 8px;
-  font-size: 14px;
-  color: #fff;
-`;
-
-const FormInput = styled.input`
-  padding: 16px 16px;
-  margin-bottom: 32px;
-  border: none;
-  border-radius: 4px;
-`;
-
 const FormButton = styled.button`
-  background: #01bf71;
-  padding: 16px 0;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background: transparent;
   border: none;
-  border-radius: 4px;
-  color: #fff;
-  font-size: 20px;
   cursor: pointer;
-`;
-
-const Text = styled.span`
-  text-align: center;
-  margin-top: 24px;
-  color: #fff;
-  font-size: 14px;
+  height: 100%;
+  width: 100%;
 `;
 
 const Signins = () => {
+  const [userLabel, setUserLabel] = useState(false);
+  const [userValue, setUserValue] = useState("");
+  const [passLabel, setPassLabel] = useState(false);
+  const [passValue, setPassValue] = useState("");
+
+  const handleParamUser = () => (e) => {
+    const userValue = e.target.value;
+    setUserValue(userValue);
+  };
+  const handleParamPass = () => (e) => {
+    const passValue = e.target.value;
+    setPassValue(passValue);
+  };
+
   return (
     <>
       <Container>
@@ -228,22 +305,48 @@ const Signins = () => {
           <div className="cardBorder">
             holis
             <div className="cardContent">
-              <h2>Login</h2>
-              <form>
-                <div class="user-box">
-                  <input type="text" name="" required="" />
-                  <label>Username</label>
+              <a className="icon" href="/">
+                FÚTBOL
+              </a>
+              <p>Iniciar sesión</p>
+              <form action="/manager">
+                <div className="user-box">
+                  <label className={userLabel ? "inlabel" : "offlabel"}>
+                    Username
+                  </label>
+                  <input
+                    type="email"
+                    name=""
+                    required
+                    value={userValue}
+                    userValue={userValue}
+                    onFocus={() => setUserLabel(true)}
+                    onBlur={() => (userValue == "" ? setUserLabel(false) : "")}
+                    onChange={handleParamUser()}
+                  />
                 </div>
                 <div class="user-box">
-                  <input type="password" name="" required="" />
-                  <label>Password</label>
+                  <label className={passLabel ? "inlabel" : "offlabel"}>
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    name=""
+                    required
+                    value={passValue}
+                    passValue={passValue}
+                    onFocus={() => setPassLabel(true)}
+                    onBlur={() => (passValue == "" ? setPassLabel(false) : "")}
+                    onChange={handleParamPass()}
+                  />
                 </div>
-                <a href="#">
-                  <span></span>
-                  <span></span>
-                  <span></span>
-                  <span></span>
+                <a className="submitBtn">
                   Submit
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                  <FormButton type="submit" />
                 </a>
               </form>
             </div>
